a97fa370cb9d3c4994c1ae9ba638be16b6a12c80
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-05-29 08:43:02 +0100

    Future

---
+++ Sources/V4Migration/Vapor/Future.swift
@@ -0,0 +1,23 @@
+//
+//  Future.swift
+//  V4Migration
+//
+//  Created by Ondrej Rafaj on 29/05/2019.
+//
+
+import Foundation
+import Vapor
+
+
+@available(*, unavailable, renamed: "EventLoopFuture", message: "[Migration] Renamed to EventLoopFuture")
+public struct Future { }
+
+
+extension EventLoopFuture {
+    
+    @available(*, unavailable, renamed: "flatMapErrorThrowing", message: "[Migration] Renamed to EventLoopFuture")
+    public func catchMap(_ callback: @escaping (Error) throws -> Value) -> EventLoopFuture<Value> {
+        fatalError()
+    }
+    
+}
