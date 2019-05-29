3cdb6b80e1eedbce95e2a3dd6bf8af1201bedcb4
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-05-29 08:48:44 +0100

    Service

---
+++ Sources/V4Migration/Vapor/Service.swift
@@ -0,0 +1,12 @@
+//
+//  Service.swift
+//  V4Migration
+//
+//  Created by Ondrej Rafaj on 29/05/2019.
+//
+
+import Foundation
+
+
+@available(*, unavailable, message: "[Migration] Any object can be a Service, remove")
+public struct Service { }
